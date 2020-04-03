d497d77824699fecc33ebb2cadd09cc93c84ea65
Olek Senyuk <oleksandr@auxoft.com>
2020-04-02 21:02:31 -0700

    Remove chrome profile

--- resources/chrome_profiles/1password/Default/Extensions/aeblfdkhhhdcdjpifhhbdiojplfjncoa/1.18.0_0/inline/i18n/messages.d.ts
+++
@@ -1,8 +0,0 @@
-declare type MessageFunction = (d: any) => string;
-
-declare interface CompiledMessages {
-  [s: string]: MessageFunction | CompiledMessages;
-}
-
-declare let messages: CompiledMessages;
-export default messages;

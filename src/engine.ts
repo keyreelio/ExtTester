6f65f3fb8c6c114a2901746e7b9fe2b4bb2c2d8c
Petro Ladkin <petro.ladkin@gmail.com>
2020-04-07 00:39:06 +0300

    - added create chrome profiles for extensions

--- src/engine.ts
+++ src/engine.ts
@@ -1,7 +1,14 @@
-import {Builder, By, until, WebDriver} from "selenium-webdriver";
+import {Builder, By, Key, until, WebDriver, WebElement} from "selenium-webdriver";
 import * as chrome from "selenium-webdriver/chrome";
 import fs from "fs";
 
+
+async function sendKeys(driver: WebDriver, elm: WebElement, value: string) {
+    for (var i = 0; i < value.length; i++) {
+        await elm.sendKeys(value[i]);
+        await driver.sleep(100);
+    }
+}
 
 export class Engine {
 
@@ -22,7 +29,11 @@ export class Engine {
         return this.createDriver(options);
     }
 
-    public async trySave(): Promise<boolean> {
+    public async processBeforeLogin(): Promise<boolean> {
+        return false;
+    }
+
+    public async processAfterLogin(): Promise<boolean> {
         return false;
     }
 }
@@ -30,65 +41,46 @@ export class Engine {
 export class KeyReelEngine extends Engine {
     public async startup(): Promise<WebDriver> {
         let options = new chrome.Options();
+        options.addArguments("user-data-dir=./chrome_profiles/keyreel");
         let crx = fs.readFileSync('./resources/crxs/keyreel.crx', {encoding: "base64"});
-        options.addExtensions(crx);
-
-        return this.createDriver(options);
-    }
-}
-
-export class LastPassEngine extends Engine {
-    public async startup(): Promise<WebDriver> {
-        let options = new chrome.Options();
-        let crx = fs.readFileSync('./resources/crxs/lastpass.crx', {encoding: "base64"});
         options.addExtensions(crx);
 
         let driver = await this.createDriver(options);
 
-        while (true) {
-            try {
-                await driver.get('chrome-extension://hdokiejnpimakedhajhdlcegeplioahd/extensionLogin.html')
-                await driver.wait(until.elementLocated(By.id("loginDialogEmail")), 5000);
-                break;
-            } catch (UnhandledPromiseRejectionWarning) {
-            }
-        }
-
-        await driver.findElement(By.id("loginDialogEmail")).sendKeys("hdayfg6wq5sq@gmail.com");
-        await driver.findElement(By.id("loginDialogPassword")).sendKeys("QL=25LXd%NT2ca5");
-        await driver.findElement(By.id("logInButton")).click();
-
-        //await driver.wait(until.("/signin"));
-        // await driver.wait(until.elementLocated(By.id("")));
+        await driver.sleep(1000);
 
         return Promise.resolve(driver);
     }
 }
 
-export class OnePasswordXEngine extends Engine {
+export class LastPassEngine extends Engine {
+
     public async startup(): Promise<WebDriver> {
         let options = new chrome.Options();
-        let crx = fs.readFileSync('./resources/crxs/1passwordx.crx', {encoding: "base64"});
+        options.addArguments("user-data-dir=./chrome_profiles/lastpass");
+        let crx = fs.readFileSync('./resources/crxs/lastpass.crx', {encoding: "base64"});
         options.addExtensions(crx);
+        let krcrx = fs.readFileSync('./resources/crxs/keyreel.crx', {encoding: "base64"});
+        options.addExtensions(krcrx);
 
         let driver = await this.createDriver(options);
 
-        while (true) {
-            try {
-                await driver.get('chrome-extension://aeblfdkhhhdcdjpifhhbdiojplfjncoa/app/app.html#/page/welcome')
-                await driver.wait(until.elementLocated(By.id("signInButton")), 5000);
-                break;
-            } catch (UnhandledPromiseRejectionWarning) {
-            }
+        await driver.sleep(1000);
+
+        await driver.get('chrome-extension://hdokiejnpimakedhajhdlcegeplioahd/extensionLogin.html');
+        try {
+            await driver.wait(until.urlContains("/extensionLogin.html"), 1000);
+            await driver.wait(until.elementLocated(By.id("loginDialogEmail")), 1000);
+
+            await driver.findElement(By.id("loginDialogEmail")).sendKeys("hdayfg6wq5sq@gmail.com");
+            await driver.findElement(By.id("loginDialogPassword")).sendKeys("QL=25LXd%NT2ca5");
+            await driver.findElement(By.id("logInButton")).click();
+        } catch (UnhandledPromiseRejectionWarning) {
         }
 
-        await driver.findElement(By.id("signInButton")).click();
-        await driver.wait(until.urlContains("/signin"));
+        //TODO: wait logged state
 
-        await driver.wait(until.elementLocated(By.id("email"))).sendKeys("hdayfg6wq5sq@gmail.com");
-        await driver.findElement(By.id("account-key")).sendKeys("A3-4J9LQG-APXJD3-KYXNL-ARBGG-TLKV3-YVMYQ");
-        await driver.findElement(By.id("master-password")).sendKeys("hrfW-Y1q3_4%");
-        await driver.findElement(By.className("signin-actions")).findElement(By.xpath("button")).click();
+        await driver.sleep(1000);
 
         return Promise.resolve(driver);
     }
@@ -97,30 +89,84 @@ export class OnePasswordXEngine extends Engine {
 export class DashlaneEngine extends Engine {
     public async startup(): Promise<WebDriver> {
         let options = new chrome.Options();
+        options.addArguments("user-data-dir=./chrome_profiles/dashlane");
         let crx = fs.readFileSync('./resources/crxs/dashlane.crx', {encoding: "base64"});
         options.addExtensions(crx);
+        let krcrx = fs.readFileSync('./resources/crxs/keyreel.crx', {encoding: "base64"});
+        options.addExtensions(krcrx);
 
         let driver = await this.createDriver(options);
 
-        while (true) {
+        await driver.sleep(1000);
+
+        try {
+            await driver.get('chrome-extension://fdjamakpfbbddfjaooikfcpapjohcfmg/signup');
+            await driver.wait(until.urlContains("/signup"), 2000);
+            await driver.wait(until.elementLocated(By.xpath("//a[@href='/login']")), 2000).click();
+        } catch (UnhandledPromiseRejectionWarning) {
+        }
+
+        try {
+            await driver.wait(until.urlContains("/login"), 2000);
+            await driver.wait(until.elementLocated(By.xpath("//input[@type='email']")), 5000).sendKeys("hdayfg6wq5sq@gmail.com");
+            await driver.findElement(By.xpath("//button[@type='button']")).click();
+        } catch (UnhandledPromiseRejectionWarning) {
+        }
+
+        try {
+            let pass = await driver.wait(until.elementLocated(By.xpath("//input[@type='password']")), 20000);
+            await sendKeys(driver, pass, "dzM+T+y(Tn~J86{");
+            //await pass.sendKeys("dzM+T+y(Tn~J86{");
+            await pass.sendKeys(Key.RETURN);
+            //await driver.findElement(By.xpath("//button[@type='button']")).click();//sendKeys(Key.RETURN);
+        } catch (UnhandledPromiseRejectionWarning) {
+        }
+
+        //TODO: wait logged state
+
+        await driver.sleep(2000);
+
+        return Promise.resolve(driver);
+    }
+}
+
+export class OnePasswordXEngine extends Engine {
+    public async startup(): Promise<WebDriver> {
+        let options = new chrome.Options();
+        options.addArguments("user-data-dir=./chrome_profiles/onepassword");
+        let crx = fs.readFileSync('./resources/crxs/1passwordx.crx', {encoding: "base64"});
+        options.addExtensions(crx);
+        let krcrx = fs.readFileSync('./resources/crxs/keyreel.crx', {encoding: "base64"});
+        options.addExtensions(krcrx);
+
+        let driver = await this.createDriver(options);
+
+        await driver.sleep(1000);
+
+        var welcome = false;
+        try {
+            await driver.get('chrome-extension://aeblfdkhhhdcdjpifhhbdiojplfjncoa/app/app.html#/page/welcome')
+            await driver.wait(until.elementLocated(By.id("signInButton")), 2000).click();
+            await driver.wait(until.urlContains("/signin"), 2000);
+
+            await driver.wait(until.elementLocated(By.id("email")), 2000).sendKeys("hdayfg6wq5sq@gmail.com");
+            await driver.findElement(By.id("account-key")).sendKeys("A3-4J9LQG-APXJD3-KYXNL-ARBGG-TLKV3-YVMYQ");
+            await driver.findElement(By.id("master-password")).sendKeys("hrfW-Y1q3_4%");
+            await driver.findElement(By.className("signin-actions")).findElement(By.xpath("button")).click();
+
+            welcome = true;
+        } catch (UnhandledPromiseRejectionWarning) {
+        }
+
+        if (!welcome) {
             try {
-                await driver.get('chrome-extension://fdjamakpfbbddfjaooikfcpapjohcfmg/signup')
-                await driver.wait(until.elementLocated(By.xpath("//input[@type='email']")), 5000);
-                break;
+                await driver.get('chrome-extension://aeblfdkhhhdcdjpifhhbdiojplfjncoa/mini/mini.html');
+                await driver.wait(until.elementLocated(By.id("masterPassword-input")), 2000).sendKeys("hrfW-Y1q3_4%", Key.RETURN);
             } catch (UnhandledPromiseRejectionWarning) {
             }
         }
 
-        await driver.wait(until.elementLocated(By.xpath("//a[@href='/login']"))).click();
-        await driver.wait(until.urlContains("/login"));
-
-        await driver.wait(until.elementLocated(By.xpath("//input[@type='email']"))).sendKeys("hdayfg6wq5sq@gmail.com");
-        await driver.findElement(By.xpath("//button[@type='button']")).click();
-
-        //TODO: for new installed extension need type code from email
-
-        await driver.wait(until.elementLocated(By.xpath("//input[@type='password']"))).sendKeys("dzM+T+y(Tn~J86{");
-        await driver.findElement(By.xpath("//button[@type='submit']")).click();
+        await driver.sleep(2000);
 
         return Promise.resolve(driver);
     }
--- src/tester.ts
+++ src/tester.ts
@@ -11,40 +11,9 @@ class Credential {
 }
 
 class TwitterCredential extends Credential{
-    public url: string = "https://twitter.com/explore";
+    public url: string = "https://twitter.com/login";
     public login: string = "hdayfg6wq5sq@gmail.com";
     public password: string = "5lKZfBc@L^PG";
-}
-
-
-class Parser {
-    public async parse(driver: WebDriver): Promise<boolean> {
-
-        let url = await driver.getCurrentUrl();
-
-        //if (url.includes("twitter")) {
-            return await this.parseForTwitter(driver);
-        //}
-
-        return Promise.resolve(false);
-    }
-
-    protected async parseForTwitter(driver: WebDriver): Promise<boolean> {
-
-        let singinQuery = "ebss = document.querySelectorAll(\"a[href='/login']\"); if (ebss.length > 0) { ebss[0].setAttribute('axt-button-type', 'singin'); }";
-        await driver.executeScript(singinQuery);
-
-        let usernameQuery = "eils = document.querySelectorAll(\"input[name='session[username_or_email]']\"); if (eils.length > 0) { eils[0].setAttribute('axt-input-type', 'login'); }";
-        await driver.executeScript(usernameQuery);
-
-        let passwordQuery = "eips = document.querySelectorAll(\"input[name='session[password]']\"); if (eips.length > 0) { eips[0].setAttribute('axt-input-type', 'password'); }";
-        await driver.executeScript(passwordQuery);
-
-        let loginQuery = "ebls = document.querySelectorAll(\"div[data-testid='LoginForm_Login_Button']\"); if (ebls.length > 0) { ebls[0].setAttribute('axt-button-type', 'login'); }";
-        await driver.executeScript(loginQuery);
-
-        return Promise.resolve(true);
-    }
 }
 
 
@@ -62,55 +31,197 @@ class Test {
 
     public async checkWriteCredential(): Promise<boolean> {
 
-        if (!await this.open(this.credential.url)) return Promise.resolve(false);
-        if (!await this.parse()) return Promise.resolve(false);
+        // try {
+            await this.driver.manage().deleteAllCookies();
+        // } catch (e) {
+        // }
 
-        if (await this.hasSigninButton()) {
-            if (!await this.clickSignInButton()) return Promise.resolve(false);
+        await this.open(this.credential.url)
+        if (!await this.hasOpened()) return Promise.resolve(false);
+
+        var enterFiledCount = 0;
+        while (true) {
+            if (!await this.parse()) return Promise.resolve(false);
+            if (await this.hasLoginForm()) {
+                if (await this.hasLogin()) {
+                    if (!await this.enterLogin(this.credential.login)) return Promise.resolve(false);
+                    enterFiledCount += 1;
+                }
+                if (await this.hasPassword()) {
+                    if (!await this.enterPassword(this.credential.password)) return Promise.resolve(false);
+                    enterFiledCount += 1;
+                }
+                if (enterFiledCount == 2 && await this.hasLoginButton()) {
+                } else {
+                    return Promise.resolve(false);
+                }
+            }
+
+            break;
         }
 
-        return true;
+        await this.engine.processBeforeLogin();
+
+        if (!await this.clickLoginButton()) return Promise.resolve(false);
+        if (!await this.parse()) return Promise.resolve(false);
+
+        if (await this.isLoggedIn()) {
+            await this.engine.processAfterLogin();
+        }
+
+        return Promise.resolve(true);
     }
 
     public async checkReadCredential(): Promise<boolean> {
-
-
-
-        return true;
+        return Promise.resolve(false);
     }
 
     protected async open(url: string): Promise<boolean> {
+        console.log("  open");
+
+        var tabs = await this.driver.getAllWindowHandles();
+        if (tabs.length === 0) {
+            await this.driver.executeScript("window.open();");
+            tabs = await this.driver.getAllWindowHandles();
+        }
+        await this.driver.switchTo().window(tabs[tabs.length - 1]);
+
+        await this.driver.get(url);
+
+        return Promise.resolve(true);
+    }
+
+    protected async hasOpened() {
+        console.log("  hasOpened");
 
         try {
             let driver = this.driver;
-
-            await this.driver.executeScript("window.open()");
-            let tabs = await this.driver.getAllWindowHandles();
-            await this.driver.switchTo().window(tabs[tabs.length - 1]);
-            await this.driver.get(url);
-
             let result = await this.driver.wait(function() {
                 return driver.executeScript('return document.readyState').then(readyState => readyState === 'complete');
             }, 5000);
         } catch (UnhandledPromiseRejectionWarning) {
-            return false;
+            return Promise.resolve(false);
         }
 
-        return true;
+        return Promise.resolve(true);
     }
 
+    //axt-parser-timing
     protected async parse(): Promise<boolean> {
-        let parser = new Parser();
+        console.log("  parse");
+        try {
+            let result = await this.driver.wait(until.elementLocated(By.xpath("//body[@axt-parser-timing]")), 5000);
+        } catch (UnhandledPromiseRejectionWarning) {
+            return Promise.resolve(false);
+        }
 
-        return await parser.parse(this.driver);
+        return Promise.resolve(true);
+    }
+
+    //axt-form-type="login"
+    protected async hasLoginForm(): Promise<boolean> {
+        console.log("  hasLoginForm");
+        try {
+            await this.driver.wait(until.elementLocated(By.xpath("//*[@axt-form-type='login']")), 5000);
+        } catch (UnhandledPromiseRejectionWarning) {
+            console.log("  login form not found");
+            return Promise.resolve(false);
+        }
+        return Promise.resolve(true);
+    }
+
+    //axt-input-type="login"
+    protected async hasLogin(): Promise<boolean> {
+        console.log("  hasLogin");
+        try {
+            await this.driver.wait(until.elementLocated(By.xpath("//input[@axt-input-type='login']")), 5000);
+        } catch (UnhandledPromiseRejectionWarning) {
+            console.log("  login input not found");
+            return Promise.resolve(false);
+        }
+        return Promise.resolve(true);
+    }
+
+    protected async enterLogin(login: string): Promise<boolean> {
+        console.log("  enterLogin");
+        try {
+            await this.driver.wait(until.elementLocated(By.xpath("//input[@axt-input-type='login']")), 5000).sendKeys(login);
+        } catch (UnhandledPromiseRejectionWarning) {
+            console.log("  login input not found");
+            return Promise.resolve(false);
+        }
+        return Promise.resolve(true);
+    }
+
+    //axt-input-type="password"
+    protected async hasPassword(): Promise<boolean> {
+        console.log("  hasPassword");
+        try {
+            await this.driver.wait(until.elementLocated(By.xpath("//input[@axt-input-type='password']")), 5000);
+        } catch (UnhandledPromiseRejectionWarning) {
+            console.log("  password input not found");
+            return Promise.resolve(false);
+        }
+        return Promise.resolve(true);
+    }
+
+    protected async enterPassword(password: string): Promise<boolean> {
+        console.log("  enterPassword");
+        try {
+            await this.driver.wait(until.elementLocated(By.xpath("//input[@axt-input-type='password']")), 5000).sendKeys(password);
+        } catch (UnhandledPromiseRejectionWarning) {
+            console.log("  password input not found");
+            return Promise.resolve(false);
+        }
+        return Promise.resolve(true);
+    }
+
+    //axt-button-type="login"
+    protected async hasLoginButton(): Promise<boolean> {
+        console.log("  hasLoginButton");
+        try {
+            await this.driver.wait(until.elementLocated(By.xpath("//*[@axt-button-type='login']")), 5000);
+        } catch (UnhandledPromiseRejectionWarning) {
+            console.log("  login button not found");
+            return Promise.resolve(false);
+        }
+        return Promise.resolve(true);
+    }
+
+    protected async clickLoginButton(): Promise<boolean> {
+        console.log("  clickLoginButton");
+        try {
+            await this.driver.wait(until.elementLocated(By.xpath("//*[@axt-button-type='login']")), 5000).click();
+        } catch (UnhandledPromiseRejectionWarning) {
+            console.log("  login button not found");
+            return Promise.resolve(false);
+        }
+        return Promise.resolve(true);
+    }
+
+    protected async hasNextButton(): Promise<boolean> {
+        console.log("  hasNextButton");
+        return Promise.resolve(false);
+    }
+
+    protected async clickNextButton(): Promise<boolean> {
+        console.log("  clickNextButton");
+        return Promise.resolve(false);
     }
 
     protected async hasSigninButton(): Promise<boolean> {
-        return true;
+        console.log("  hasSigninButton");
+        return Promise.resolve(false);
     }
 
     protected async clickSignInButton(): Promise<boolean> {
-        return true;
+        console.log("  clickSignInButton");
+        return Promise.resolve(false);
+    }
+
+    protected async isLoggedIn(): Promise<boolean> {
+        console.log("  isLoggedIn");
+        return Promise.resolve(false);
     }
 }
 
@@ -137,46 +248,46 @@ async function testExecute(engine: Engine): Promise<void> {
     // create WebDriver with extension
     let driver = await engine.startup();
 
-    // for (let credential of credentials) {
-    //     let api = new Test(driver, engine, credential);
+    for (let credential of credentials) {
+        let api = new Test(driver, engine, credential);
+
+        console.log(`testing...  '${credential.url}'`);
+        if (await api.checkWriteCredential()) {
+            console.log("  did write credential");
+            if (await api.checkReadCredential()) {
+                console.log("  did read credential");
+            } else {
+                console.log("  did fail read credential");
+            }
+        } else {
+            console.log("  did fail write credential");
+        }
+    }
+
+    // // run twitter test
+    // //  write credential
+    // await driver.executeScript("window.open()");
+    // let tabs = await driver.getAllWindowHandles();
+    // await driver.switchTo().window(tabs[tabs.length - 1]);
+    // await driver.get('https://twitter.com/login');
+    // await driver.wait(until.elementLocated(By.name("session[username_or_email]")), 1000).sendKeys("hdayfg6wq5sq@gmail.com");
+    // await driver.findElement(By.name("session[password]")).sendKeys("5lKZfBc@L^PG");
     //
-    //     console.log(`testing...  '${credential.url}'`);
-    //     if (await api.checkWriteCredential()) {
-    //         console.log("  did write credential");
-    //         if (await api.checkReadCredential()) {
-    //             console.log("  did read credential");
-    //         } else {
-    //             console.log("  did fail read credential");
-    //         }
-    //     } else {
-    //         console.log("  did fail write credential");
-    //     }
-    // }
-
-    // run twitter test
-    //  write credential
-    await driver.executeScript("window.open()");
-    let tabs = await driver.getAllWindowHandles();
-    await driver.switchTo().window(tabs[tabs.length - 1]);
-    await driver.get('https://twitter.com/login');
-    await driver.wait(until.elementLocated(By.name("session[username_or_email]")), 1000).sendKeys("hdayfg6wq5sq@gmail.com");
-    await driver.findElement(By.name("session[password]")).sendKeys("5lKZfBc@L^PG");
-
-    //await engine.prevSave();
-
-    await driver.findElement(By.xpath("//div[@data-testid='LoginForm_Login_Button']")).click();
-
-    //await engine.postSave();
-
-    //await engine.hasSaved();
-
-    // read credential
-    await driver.manage().deleteAllCookies();
-
-    await driver.executeScript("window.open()");
-    let tabs2 = await driver.getAllWindowHandles();
-    await driver.switchTo().window(tabs2[tabs2.length - 1]);
-    await driver.get('https://twitter.com/login');
+    // //await engine.prevSave();
+    //
+    // await driver.findElement(By.xpath("//div[@data-testid='LoginForm_Login_Button']")).click();
+    //
+    // //await engine.postSave();
+    //
+    // //await engine.hasSaved();
+    //
+    // // read credential
+    // await driver.manage().deleteAllCookies();
+    //
+    // await driver.executeScript("window.open()");
+    // let tabs2 = await driver.getAllWindowHandles();
+    // await driver.switchTo().window(tabs2[tabs2.length - 1]);
+    // await driver.get('https://twitter.com/login');
 
     //await engine.load();
 
@@ -195,10 +306,12 @@ async function test() : Promise<void> {
     //     testExecute(new DashlaneEngine()),
     //     testExecute(new OnePasswordXEngine())]);
 
-    await Promise.all([testExecute(new LastPassEngine())]);
+    await Promise.all([testExecute(new DashlaneEngine())]);
 
     console.log('...stop');
 };
 
+console.log(process.argv);
 
 test();
+
